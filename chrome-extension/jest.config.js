b494bd6d24da332f6fa1dd8a43e6251ed6e18f44
Stevemech <stevemech2020@gmail.com>
2026-05-31 19:18:17 -0700

    removed chrome extension

--- chrome-extension/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  testEnvironment: "jest-environment-jsdom",
-  testMatch: ["<rootDir>/tests/**/*.test.js"],
-  setupFiles: ["<rootDir>/tests/setup.js"],
-  testTimeout: 15000,
-};
